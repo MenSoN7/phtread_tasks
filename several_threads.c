55d69c748cc06eb1d7fff19cffcc4fa3eb9775ec
Minaev Vitaliy <113594604+MenSoN7@users.noreply.github.com>
2022-10-28 20:03:38 +0300

    Update several_threads.c

--- several_threads.c
+++ several_threads.c
@@ -7,105 +7,102 @@
 #include <math.h>
 #include <unistd.h>
 
-/* #define P 1
-#define Q 800000 */
-
 long long int sum = 0; // общие данные для потоков  - переменная для подсчета суммы делителей
 pthread_mutex_t mutex;
 
 typedef struct interval
 {
-  long int p;
-  long int q;
+    long int p;
+    long int q;
 } interval_t;
 
 void number_divisors(long int n); // функция для подсчета суммы делителей числа n
-void *thread_job(void *param); // потоковая функция  
+void *thread_job(void *param); // потоковая функция
 
-int main(int argc, char *argv[]) 
-{
-  if (argc != 3) 
-  {
-    printf("Неверное количество аргументов!\n");
-    return -1;
-  }
+int main(int argc, char *argv[]) {
 
-  if (atoi(argv[1]) > atoi(argv[2])) 
-  {
-    printf("Первый аргумент должен быть меньше второго!\n");
-    return -1;
-  }
+    if (argc != 3)
+    {
+        printf("Неверное количество аргументов!\n");
+        return -1;
+    }
 
-  //узнаем количество ядер
-  long int count_of_cores = sysconf(_SC_NPROCESSORS_CONF);
+    if (atoi(argv[1]) > atoi(argv[2]))
+    {
+        printf("Первый аргумент должен быть меньше второго!\n");
+        return -1;
+    }
 
-  //выделение памяти для n-ого количества интервалов, где n - количество ядер
-  //(переменная count_of_cores)
-  interval_t* intervals;
-  intervals = (interval_t *) malloc(count_of_cores * sizeof(interval_t));
+    //узнаем количество ядер
+    long int count_of_cores = sysconf(_SC_NPROCESSORS_CONF);
 
-  //выделение памяти для n-ого количества потоков, где n - количество ядер
-  //(переменная count_of_cores)
-  pthread_t *p;       
-  p = (pthread_t *) malloc(count_of_cores * sizeof(pthread_t));
+    //выделение памяти для n-ого количества интервалов, где n - количество ядер
+    //(переменная count_of_cores)
+    interval_t* intervals;
+    intervals = (interval_t *) malloc(count_of_cores * sizeof(interval_t));
 
-  long int P = atoi(argv[1]);
-  long int Q = atoi(argv[2]);
+    //выделение памяти для n-ого количества потоков, где n - количество ядер
+    //(переменная count_of_cores)
+    pthread_t *p;
+    p = (pthread_t *) malloc(count_of_cores * sizeof(pthread_t));
 
-  //инициализация мьютекса
-  pthread_mutex_init(&mutex,NULL);
+    long int P = atoi(argv[1]);
+    long int Q = atoi(argv[2]);
 
-  for (int i=0; i < count_of_cores; ++i)
+    //инициализация мьютекса
+    pthread_mutex_init(&mutex,NULL);
+
+    for (int i=0; i < count_of_cores; ++i)
     {
         intervals[i].p = P+i;
         intervals[i].q = Q;
         pthread_create(&p[i],NULL,thread_job,&intervals[i]);
     }
 
-  for(int i=0; i < count_of_cores ; ++i)
+    for(int i=0; i < count_of_cores ; ++i)
         pthread_join(p[i],NULL);
-    
-  pthread_mutex_destroy(&mutex);
-  printf("Count of divisors = %lld\n", sum);
 
-  //освобождаем выделенную память
-  free(p);
-  free(intervals);
+    pthread_mutex_destroy(&mutex);
+    printf("Count of divisors = %lld\n", sum);
 
-  return 0;
+    //освобождаем выделенную память
+    free(p);
+    free(intervals);
+
+    return 0;
 }
 
-// Потоковая функция 
-void *thread_job(void *interval) 
+// Потоковая функция
+void *thread_job(void *interval)
 {
-  //узнаем количество ядер
-  long int count_of_cores = sysconf(_SC_NPROCESSORS_CONF);
-  interval_t*  interval_= (interval_t*) interval;
-  for (long int i = interval_->p; i <= interval_->q; i+=count_of_cores)
-  {
-     number_divisors(i);
-  }
-  return NULL;
+    //узнаем количество ядер
+    long int count_of_cores = sysconf(_SC_NPROCESSORS_CONF);
+    interval_t*  interval_= (interval_t*) interval;
+    for (long int i = interval_->p; i <= interval_->q; i+=count_of_cores)
+    {
+        number_divisors(i);
+    }
+    return NULL;
 }
 
 //функция, которая считает количество делителей для числа
-void number_divisors(long int n) 
-{
- for (long int j = 1; j <= sqrt(n); ++j)
-      {
+void number_divisors(long int n) {
+
+    for (long int j = 1; j <= sqrt(n); ++j)
+    {
         if (!(n % j))
-         {
-           pthread_mutex_lock(&mutex);
-           if (j != n/j)
+        {
+            pthread_mutex_lock(&mutex);
+            if (j != n/j)
             {
-             sum += 2;             
+                sum += 2;
             }
             else
             {
-             sum++;     
+                sum++;
             }
-           pthread_mutex_unlock(&mutex);
-         }
-      } 
-  
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+
 }
